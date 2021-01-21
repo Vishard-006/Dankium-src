0d35be73f3870b407c83d8a42840e84eb5db4036
Vishard_006 <vishal06krish@gmail.com>
2021-01-21 19:42:29 +0530

    added import os

--- dank_memer.py
+++ dank_memer.py
@@ -1,7 +1,9 @@
 from selenium import webdriver
 import time
+import os
  
 browser = webdriver.Chrome(executable_path='C:/Chrome-Web-Driver/chromedriver.exe')
 browser.get("https://discord.com/channels/753183692490276924/770141397063696404")
+
 
 
