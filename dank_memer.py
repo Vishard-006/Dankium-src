f1caf6d790646507b2b3a6688bf735351b8da7b4
Vishard_006 <vishal06krish@gmail.com>
2021-01-21 20:06:48 +0530

    added custom URL

--- dank_memer.py
+++ dank_memer.py
@@ -3,7 +3,7 @@ import time
 import os
  
 browser = webdriver.Chrome(executable_path='C:/Chrome-Web-Driver/chromedriver.exe')
-browser.get("https://discord.com/channels/753183692490276924/770141397063696404")
+browser.get("Your URL Here")
 
 
 
