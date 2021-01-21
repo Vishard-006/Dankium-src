3f97b456597ef2ed2c2677d2fcdae002a94339b7
Vishard_006 <vishal06krish@gmail.com>
2021-01-21 19:37:52 +0530

    First commit

---
+++ dank_memer.py
@@ -0,0 +1,7 @@
+from selenium import webdriver
+import time
+ 
+browser = webdriver.Chrome(executable_path='C:/Chrome-Web-Driver/chromedriver.exe')
+browser.get("https://discord.com/channels/753183692490276924/770141397063696404")
+
+
