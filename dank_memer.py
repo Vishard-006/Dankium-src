23696632a437bcecf7943b8501354f2aac87c532
Vishard_006 <vishal06krish@gmail.com>
2021-01-22 15:14:30 +0530

    added login

--- dank_memer.py
+++ dank_memer.py
@@ -1,13 +1,19 @@
 from selenium import webdriver
 import getpass
 import time
-driver_path = input("Enter Chrome Web-Driver Path  : ")
+
 uName = input("Enter Discord Username / Email :")
 uPwd = getpass.getpass("Enter PWD :")
 url = input("Enter Server Channel Url:")
 if (url.startswith("https://discord.com/"))  :
-    browser = webdriver.Chrome(executable_path=driver_path)
-    browser.get(url)
+    driver = webdriver.Chrome(executable_path='C:\Chrome-Web-Driver\chromedriver.exe')
+    driver.get(url)
+    email = driver.find_element_by_name('email')
+    email.send_keys(uName)
+    password = driver.find_element_by_name('password')
+    password.send_keys(uPwd)
+    login_button = driver.find_element_by_xpath('//*[@id="app-mount"]/div[2]/div/div[2]/div/div/form/div/div/div[1]/div[3]/button[2]')
+    login_button.click()
     
 
 else  :
