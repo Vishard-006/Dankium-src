74999623c5fc93f67d24af02def578ea091c318f
Vishard_006 <vishal06krish@gmail.com>
2021-01-22 20:02:07 +0530

    added basically all features

--- dank_memer.py
+++ dank_memer.py
@@ -1,10 +1,19 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import getpass
 import time
+import random
+import csv
+from pathlib import Path
 
+
+my_file = Path("./usr.csv")
+if my_file.is_file() :
+    Uname = 
 uName = input("Enter Discord Username / Email :")
 uPwd = getpass.getpass("Enter PWD :")
 url = input("Enter Server Channel Url:")
+
 if (url.startswith("https://discord.com/"))  :
     driver = webdriver.Chrome(executable_path='C:\Chrome-Web-Driver\chromedriver.exe')
     driver.get(url)
@@ -12,11 +21,43 @@ if (url.startswith("https://discord.com/"))  :
     email.send_keys(uName)
     password = driver.find_element_by_name('password')
     password.send_keys(uPwd)
+    
     login_button = driver.find_element_by_xpath('//*[@id="app-mount"]/div[2]/div/div[2]/div/div/form/div/div/div[1]/div[3]/button[2]')
     login_button.click()
-    a = driver.find_elements_by_xpath('//*[@id="app-mount"]/div[2]/div/div[2]/div/div/div/div/div[2]/div/main/form/div[1]/div/div/div/div[3]/div[2]')
-    a.send_keys("hello")
+    time.sleep(10)
+    msg = driver.find_element_by_xpath('//*[@id="app-mount"]/div[2]/div/div[2]/div/div/div/div/div[2]/div/main/form/div[1]/div/div/div/div[3]/div[2]')
+    print ("This program will run on an infinite loop . Press CTRL+C anytime to stop")
     
+    while True :
+        msg.send_keys("pls beg")
+        msg.send_keys(Keys.RETURN)
+        time.sleep(5)
+        msg.send_keys("pls dep all")
+        msg.send_keys(Keys.RETURN)
+        time.sleep(5)
+        msg.send_keys("pls hunt")
+        msg.send_keys(Keys.RETURN)
+        time.sleep(5)
+        msg.send_keys("pls fish")
+        msg.send_keys(Keys.RETURN)
+        time.sleep(5)
+        memes = ['f' , 'r' , 'i' , 'c' , 'k']
+        pm = random.choice(memes)
+        msg.send_keys("pls pm")
+        msg.send_keys(Keys.RETURN)
+        time.sleep(5)
+        msg.send_keys(pm)
+        msg.send_keys(Keys.RETURN)
+        time.sleep(5)
+        msg.send_keys("pls dep all")
+        msg.send_keys(Keys.RETURN)
+        time.sleep(5)
+        my_file = Path("/path/to/file")
+        msg.send_keys("pls search")
+        search = driver.find_element_by_xpath('//*[@id="chat-messages-802145332174520381"]/div[1]/div').text
+        msg.send_keys(search)
+        msg.send_keys(Keys.RETURN)
+        time.sleep(5)
 
 else  :
     print ("URL invalid")
