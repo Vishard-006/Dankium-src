d065d8c053efc3532c1d4bdc4b8f4a4049a6a938
Vishard_006 <vishal06krish@gmail.com>
2021-01-22 10:50:10 +0530

    added url checking

--- dank_memer.py
+++ dank_memer.py
@@ -1,9 +1,17 @@
 from selenium import webdriver
+import getpass
 import time
+uName = input("Enter Discord Username :")
+uPwd = getpass.getpass("Enter PWD :")
+url = input("Enter Server Channel Url")
+if (url.startswith("https://"))  :
+    browser = webdriver.Chrome(executable_path='C:/Chrome-Web-Driver/chromedriver.exe')
+    browser.get(url)
 
+
+else  :
+    print ("URL invalid")
  
-browser = webdriver.Chrome(executable_path='C:/Chrome-Web-Driver/chromedriver.exe')
-browser.get("Your URL Here")
 
 
 
