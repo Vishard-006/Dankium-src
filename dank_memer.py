6dcf3c86326ecf82b852889d959239ff54530462
Vishard_006 <vishal06krish@gmail.com>
2021-01-22 15:00:54 +0530

    propmt for chrome_driver

--- dank_memer.py
+++ dank_memer.py
@@ -1,13 +1,14 @@
 from selenium import webdriver
 import getpass
 import time
+driver_path = input("Enter Chrome Web-Driver Path  : ")
 uName = input("Enter Discord Username / Email :")
 uPwd = getpass.getpass("Enter PWD :")
 url = input("Enter Server Channel Url:")
 if (url.startswith("https://discord.com/"))  :
-    browser = webdriver.Chrome(executable_path='C:/Chrome-Web-Driver/chromedriver.exe')
+    browser = webdriver.Chrome(executable_path=driver_path)
     browser.get(url)
-    print("THANKS")
+    
 
 else  :
     print ("URL invalid")
