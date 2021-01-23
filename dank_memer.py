e59bff6373da97cec65d001fb9e0ef3e5aff1376
Vishard_006 <vishal06krish@gmail.com>
2021-01-23 13:42:46 +0530

    added 15 seconds timer and json saving

--- dank_memer.py
+++ dank_memer.py
@@ -8,10 +8,10 @@ import json
 
 
 
-if os.path.isfile("./usr.json") :
+if os.path.isfile("./data.json") :
     y = input("Load saved Login ? y/n ?") 
     if y=='y' :
-        f = open("./usr.json")
+        f = open("./data.json")
         data = json.loads(f.read())
         uName = data["Name"]
         uPwd = data["PWD"]
@@ -20,32 +20,56 @@ if os.path.isfile("./usr.json") :
         uName = input("Enter Discord Username / Email :")
         uPwd = getpass.getpass("Enter PWD :")
         url = input("Enter Server Channel Url:")
-"""
+
 else :
-    x = input("want to save login credentials ? y/n ?")
-    if x=='y' :
-        f = open("usr.json", "a+")
-        f.write("{")
-        f.wri
-        uName = input("Enter Discord Username / Email :")
-        uPwd = getpass.getpass("Enter PWD :")
-        url = input("Enter Server Channel Url:")
-        
-        data = json.loads(f.read())
-        data["Name"] = uName
-        data["PWD"] = uPwd
-        data["url"] = url
-    else :
+    x = input("want to save login credentials (credentials will be stored at ./data.json) ? y/n ?")
+    if x=='y' :     
             uName = input("Enter Discord Username / Email :")
             uPwd = getpass.getpass("Enter PWD :")
-            url = input("Enter Server Channel Url:"
-"""
+            url = input("Enter Server Channel Url:")
+            f = open("usr.json", "a+")
+            f.write("{")
+            f.write("\n")
+            f.write('"Name"')
+            f.write(" : ")
+            f.write('"x" ,')
+            f.write("\n")
+            f.write('"PWD"')
+            f.write(" : ")
+            f.write('"y" ,')
+            f.write("\n")
+            f.write('"url"')
+            f.write(" : ")
+            f.write('"z"')
+            f.write("\n")
+            f.write("}")
+           
+            f = open("./usr.json" , "a+")
+            data = json.loads(f.read())
+            data["Name"] = uName
+            data["PWD"] = uPwd
+            data["url"] = url
+            
+            f.close()
+            os.remove("./usr.json")
+            d = open("data.json" , "a+")
+            json.dump(data, d)
+            
+    else :
+         uName = input("Enter Discord Username / Email :")
+         uPwd = getpass.getpass("Enter PWD :")
+         url = input("Enter Server Channel Url:")
 
 
 
 
 
-if (url.startswith("https://discord.com/"))  :
+
+if (url.startswith("https://discord.com/")):
+    xPath ='//*[@id="app-mount"]/div[2]/div/div[2]/div/div/div/div/div[2]/div/main/form/div[1]/div/div/div/div[1]/div[2]'
+    a = input("By default this bot will post to Friends Hangout . Post somewhere else ?")
+    if a=='y' :
+        xPath = input("Go to your browser dev tools , click select by element and click on message input box and select copy xpath. Now paste that value here :")
     driver = webdriver.Chrome(executable_path=r'C:\Chrome-Web-Driver\chromedriver.exe')
     driver.get(url)
     email = driver.find_element_by_name('email')
@@ -55,8 +79,8 @@ if (url.startswith("https://discord.com/"))  :
     
     login_button = driver.find_element_by_xpath('//*[@id="app-mount"]/div[2]/div/div[2]/div/div/form/div/div/div[1]/div[3]/button[2]')
     login_button.click()
-    time.sleep(20)
-    msg = driver.find_element_by_xpath('//*[@id="app-mount"]/div[2]/div/div[2]/div/div/div/div/div[2]/div/main/form/div[1]/div/div/div/div[1]/div[2]')
+    time.sleep(15)
+    msg = driver.find_element_by_xpath(xPath)
     print ("This program will run on an infinite loop . Press CTRL+C anytime to stop")
     
     
