c24bf916ea35ed8d247f447883c8d37da94d30f9
Vishard_006 <vishal06krish@gmail.com>
2021-01-23 14:23:57 +0530

    refinde

--- dank_memer.py
+++ dank_memer.py
@@ -4,9 +4,10 @@ import getpass
 import time
 import random
 import os.path
+from os import system, name
 import json
 
-
+system("cls")
 
 if os.path.isfile("./data.json") :
     y = input("Load saved Login ? y/n ?") 
@@ -66,12 +67,14 @@ else :
 
 
 if (url.startswith("https://discord.com/")):
+    system("cls")
     xPath ='//*[@id="app-mount"]/div[2]/div/div[2]/div/div/div/div/div[2]/div/main/form/div[1]/div/div/div/div[1]/div[2]'
-    a = input("By default this bot will post to Friends Hangout . Post somewhere else ?")
-    if a=='y' :
+    a = input("By default this bot will post to Friends Hangout . Continue y/n ? ")
+    if a=='n' :
         xPath = input("Go to your browser dev tools , click select by element and click on message input box and select copy xpath. Now paste that value here :")
     driver = webdriver.Chrome(executable_path=r'C:\Chrome-Web-Driver\chromedriver.exe')
     driver.get(url)
+    system("cls")
     email = driver.find_element_by_name('email')
     email.send_keys(uName)
     password = driver.find_element_by_name('password')
@@ -79,21 +82,22 @@ if (url.startswith("https://discord.com/")):
     
     login_button = driver.find_element_by_xpath('//*[@id="app-mount"]/div[2]/div/div[2]/div/div/form/div/div/div[1]/div[3]/button[2]')
     login_button.click()
-    time.sleep(15)
+    time.sleep(10)
     msg = driver.find_element_by_xpath(xPath)
-    print ("This program will run on an infinite loop . Press CTRL+C anytime to stop")
+    system("cls")
+    print ("Logged in Succesfully . If you have chosen for credentials to be saved , it will save after you press CTRL+C . This program will run on an infinite loop . Press CTRL+C anytime to stop")
     
     
     while True :
         msg.send_keys("pls beg")
         msg.send_keys(Keys.RETURN)
-        time.sleep(6)
+        time.sleep(7)
         msg.send_keys("pls dep all")
         msg.send_keys(Keys.RETURN)
-        time.sleep(6)
+        time.sleep(7)
         msg.send_keys("pls hunt")
         msg.send_keys(Keys.RETURN)
-        time.sleep(6)
+        time.sleep(7)
         msg.send_keys("pls fish")
         msg.send_keys(Keys.RETURN)
         time.sleep(7)
@@ -101,15 +105,15 @@ if (url.startswith("https://discord.com/")):
         pm = random.choice(memes)
         msg.send_keys("pls pm")
         msg.send_keys(Keys.RETURN)
-        time.sleep(6)
+        time.sleep(7)
         msg.send_keys(pm)
         msg.send_keys(Keys.RETURN)
-        time.sleep(6)
+        time.sleep(7)
         msg.send_keys("pls dep all")
         msg.send_keys(Keys.RETURN)
-        time.sleep(6)
+        time.sleep(7)
         
-        time.sleep(6)
+        time.sleep(8)
 
 else  :
     print ("URL invalid")
